17041b91d0281ef0e905ced92ded243dc946c033
Mfundo-debug <diditmfundo@gmail.com>
2024-06-24 20:17:26 +0200

    feat: Update Shiny app with data table functionality

--- dashboard/App.R
+++ dashboard/App.R
@@ -1,23 +1,19 @@
 library(shiny)
 library(bs4Dash)
+library(haven)
+library(DT)
 
+
+Educ_data <- read_sav("ELRC2004_2005_Educators_data.sav")
 ui <- dashboardPage(
   fullscreen = TRUE,
+  scrollToTop = TRUE,
   title = "Data Analytics",
   header = dashboardHeader(
     title = dashboardBrand(
       title = "Data Analytics",
       image ="UKZN-logo.jpg"
-      ),
-    rightUi = dropdownMenu(
-      badgeStatus = "info",
-      type = "notifications",
-      notificationItem(
-        text = "success",
-        status = "success",
-        icon = icon("envelope")
       )
-    )
   ),
   sidebar = dashboardSidebar(
     sidebarMenu(
@@ -27,7 +23,11 @@ ui <- dashboardPage(
       menuItem("Reports", tabName = "reports", icon = icon("file-pdf"))
     )
   ),
-  controlbar = dashboardControlbar(),
+  controlbar = dashboardControlbar(
+    collsapsible = TRUE,
+    div(class="p-3", skinSelector()),
+    title = "Skin Selector",
+  ),
   footer = dashboardFooter(
     left = "Mfundo Monchwe",
     right = "2024"
@@ -41,7 +41,7 @@ ui <- dashboardPage(
 Public Schools: A Spatial and Bayesian Analysis
 ",
                 lead = "Vsualising data with R",
-                status = "info",
+                status = "danger",
                 btnName = "Download",
                 href = "https://stuukznac-my.sharepoint.com/:f:/g/personal/robertsd_ukzn_ac_za/Em0XWZovqfJFgKcEEg9b-9cBjLRGl4ZSqSPCjAYi_NIvgQ?e=gogSwX"
               ),
@@ -51,28 +51,66 @@ Public Schools: A Spatial and Bayesian Analysis
                   collapsible = TRUE,
                   title = userDescription(
                     title = "Mfundo Monchwe",
-                    subtitle = "Data Scientist",
+                    subtitle = "Data Scientist, MSc(Statistics) candidate ",
                     image = "mfundo.jpg",
                     type = 2
                   ),
-                  status = "info",
+                  status = "danger",
                   "I am a data scientist with a passion for data analysis and visualisation. I am currently working on a project that aims to investigate the spatial distribution of HIV risk among educators in South African public schools. I am using R to visualise the data and conduct the analysis. I am also using Bayesian statistics to model the data. I am excited to share my findings with you."
                   ),
                 box (
                   title = "Data",
-                  status = "info",
+                  status = "danger",
                   solidHeader = TRUE,
                   collapsible = TRUE,
                   width = 6,
                   blockQuote(
                   "The data used in this project was obtained from the Department of Basic Education. The data contains information on the spatial distribution of educators in South African public schools. The data also contains information on the HIV status of educators. The data is stored in a CSV file and is cleaned and processed using R.",
-                  color = "danger"
+                  color = "teal"
+                  )
+                ),
+                userBox(
+                  collapsible = TRUE,
+                  title = userDescription(
+                    title = "Dr Danielle J. Roberts",
+                    subtitle = "PhD (Statistics), MSc (Statistics), BSc Honours (Statistics), BSc (Applied Mathematics & Statistics)",
+                    image = "Danielle-Roberts.jpg",
+                    type = 2
+                  ),
+                  status = "danger",
+                  "Dr Danielle Jade Roberts holds a PhD in Applied Statistics. She has vast experience in teaching, curriculum development, research and student supervision in applied statistics and data science. She takes a data-driven approach to solve real world problems using statistics, machine learning and technology, with a focus on public health and other societal challenges in resource-limited settings in Africa.",
+                  ribbon(
+                    text ="Supervisor",
+                    color = "success"
+                  )
+                ),
+                userBox(
+                  collapsible = TRUE,
+                  title = userDescription(
+                    title = "Dr Nombuso Zondo",
+                    subtitle = "PhD (Statistics), MSc (Statistics), BSc Honours (Statistics), BSc (Applied Mathematics & Statistics)",
+                    image = "Zondo.png",
+                    type = 2
+                  ),
+                  status = "danger",
+                  "Dr Nombuso Zondo is a lecturer in the discipline of Statistics and holds a PhD in Statistics. She has a passion for teaching statistics and its application in life matters. She is involved in undergraduate and postgraduate teaching in the statistics and data science streams. Her research interests have been in statistics education as well as applied statistics with the aim to cover a range of multidisciplinary applications.",
+                  ribbon(
+                    text ="Co-Supervisor",
+                    color = "primary"
                   )
                 )
                 )
       ),
       tabItem(tabName = "data",
-              h2("Data")
+              tabBox(
+                title = "Data",
+                type = "tabs",
+                width = 12,
+                status = "danger",
+                solidHeader = TRUE,
+                tabPanel("Data Table",
+              DT :: dataTableOutput("data_table"))
+              )
       ),
       tabItem(tabName = "analysis",
               h2("Analysis")
@@ -84,6 +122,9 @@ Public Schools: A Spatial and Bayesian Analysis
   )
 )
 server <- function(input, output, session) {
+  output$data_table <- DT::renderDataTable({
+    DT::datatable(Educ_data)
+  })
   
 }
 
